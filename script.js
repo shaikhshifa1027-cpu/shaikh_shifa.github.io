1a01dd6832f323e436cb845cfc79027d4e2a5aa0
shaikhshifa1027-cpu <shaikhshifa1027@gmail.com>
2026-04-06 15:31:14 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+// Smooth Scroll
+document.querySelectorAll("a").forEach(link => {
+    link.addEventListener("click", function(e) {
+        if (this.hash !== "") {
+            e.preventDefault();
+            document.querySelector(this.hash)?.scrollIntoView({
+                behavior: "smooth"
+            });
+        }
+    });
+});
+
+// Form Validation
+document.getElementById("contactForm")?.addEventListener("submit", function(e){
+    e.preventDefault();
+
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let message = document.getElementById("message").value;
+
+    if(name === "" || email === "" || message === ""){
+        document.getElementById("formMsg").innerText = "Please fill all fields!";
+    } else {
+        document.getElementById("formMsg").innerText = "Message sent successfully!";
+    }
+});
